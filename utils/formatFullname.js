5f30babbd4d32df96c41ee1de469a180b1f89efc
Anna Kijania <kijania.anna@gmail.com>
2020-12-09 21:21:52 +0100

    Fix formatFullname

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,9 +1,8 @@
 module.exports = (fullName) => {
   if (typeof fullName !== 'string') return 'Error';
-  if (!fullName.includes(' ') || fullName.split(' ').length - 1 > 1)
+  if (fullName.split(' ').length != 2)
     return 'Error';
   const [firstName, lastName] = fullName.split(' ');
-  if (!firstName || !lastName) return false;
   return (
     firstName[0].toUpperCase +
     firstName.slice(1).toLowerCase() +
